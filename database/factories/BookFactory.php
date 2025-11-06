3a61f1e4018341a4059d48ebaa51c5be2eb3546e
devnouveau <chowbiz715@gmail.com>
2025-11-06 14:59:12 +0900

    chore : 도서, 리뷰의 factory 및 seeder 정의

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookFactory extends Factory
+{
+    protected $model = Book::class;
+
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(3),
+            'author' => fake()->name,
+            'created_at' => fake()->dateTimeBetween('-2 years'),
+            'updated_at' => function (array $attributes) {
+                return fake()->dateTimeBetween($attributes['created_at'], 'now');
+            },
+        ];
+    }
+}
