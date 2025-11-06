7c2e69e452cf22f223a278685384e77df80437cf
devnouveau <chowbiz715@gmail.com>
2025-11-06 15:31:19 +0900

    feat : 도서명으로 도서를 검색할 수 있도록 로컬 스코프 추가

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,4 +15,17 @@ class Book extends Model
     {
         return $this->hasMany(Review::class);
     }
+
+    // TODO : 추후 모든 로컬 스코프 메소드 정의를 scope속성을 추가하는 방식으로 변경 필요
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+//    #[Scope]
+//    protected function title(Builder $query, string $title): void
+//    {
+//        $query->where('title', 'LIKE', '%' . $title . '%');
+//    }
+
 }
