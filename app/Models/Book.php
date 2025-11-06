c46a281686cba3c0d8e2bac6ab81689121c4b313
devnouveau <chowbiz715@gmail.com>
2025-11-06 17:25:10 +0900

    feat : 특정 기간 동안 등록된 리뷰의 수나 평균점수가 높은 순으로 도서를 조회하기 위한 로컬 스코프 메소드 추가

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -22,4 +22,39 @@ class Book extends Model
         $query->where('title', 'LIKE', '%' . $title . '%');
     }
 
+
+    #[Scope]
+    protected function popular(Builder $query, $from = null, $to = null): void // TODO: 필터링이 아니라 정렬을 위한 메소드이므로 추후 메소드명 변경 필요
+    {
+        $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
+            ->orderBy('reviews_count', 'desc'); // 특정기간 등록된 리뷰가 많은 순으로 정렬
+    }
+
+    #[Scope]
+    protected function highestRated(Builder $query, $from = null, $to = null): void // TODO: 필터링이 아니라 정렬을 위한 메소드이므로 추후 메소드명 변경 필요
+    {
+        $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc'); // 특정기간 등록된 리뷰의 평균점이 많은 순으로 정렬
+    }
+
+    #[Scope]
+    private function minReviews(Builder $query, int $minReviews): void
+    {
+        $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null): void
+    {
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
+    }
 }
