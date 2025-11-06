6ea2ccb33b6a1ace60c594ac68a12e5297b094f5
devnouveau <chowbiz715@gmail.com>
2025-11-06 18:28:32 +0900

    docs : 기능변경 혹은 이름변경에 대한 검토를 요하는 TODO 주석 작성

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -22,9 +22,15 @@ class Book extends Model
         $query->where('title', 'LIKE', '%' . $title . '%');
     }
 
-
+    /* TODO :
+        메소드명, UI 혹은 기능 자체를 변경할지 검토해야 함.
+        아래 스코프 메소드 모두 필터링이 아니라 정렬(리뷰수 순, 별점순 등)을 위한 메소드로
+        기간에 상관없이 모든 도서를 조회하게 됨.
+        그러나 UI상 노출되는 표현과 메소드명은 도서목록을 필터링해서 조회하는 기능을 의미하는 것처럼 보일 수 있으므로
+        혼란을 방지하기 위해 어떤 식으로 변경할지 검토가 필요함.
+    */
     #[Scope]
-    protected function popular(Builder $query, $from = null, $to = null): void // TODO: 필터링이 아니라 정렬을 위한 메소드이므로 추후 메소드명 변경 필요
+    protected function popular(Builder $query, $from = null, $to = null): void
     {
         $query->withCount([
             'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
@@ -33,7 +39,7 @@ class Book extends Model
     }
 
     #[Scope]
-    protected function highestRated(Builder $query, $from = null, $to = null): void // TODO: 필터링이 아니라 정렬을 위한 메소드이므로 추후 메소드명 변경 필요
+    protected function highestRated(Builder $query, $from = null, $to = null): void
     {
         $query->withAvg([
             'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
